fb5f691ef90cb7edd77977dbba9845f320ac296d
Gana <chethuuc87@gmail.com>
2018-02-11 15:07:19 +0530

    first commit

---
+++ mouse.py
@@ -0,0 +1,22 @@
+class Pointer(object):
+
+	def __init__(self,x,y):
+		self.x=x
+		self.y=y
+
+	def moveUp(self):
+		self.y += 1	
+
+	def moveDown(self):
+		self.y -= 1
+
+	def moveLeft(self):
+		self.x -= 1
+
+	def moveRight(self):
+		self.x += 1	
+
+	def check(self):
+		x=self.x
+		y=self.y
+		print(x,y)	
